47b1c1109b5a59b874efcad0c6d633db8bdd3b8c
Victorbriez <victor.briez@epitech.eu>
2024-12-21 11:30:38 +0100

    feat: add user retrieval endpoint and service method for fetching a single user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,6 +4,7 @@ import { PrismaService } from 'src/prisma.service';
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
+
   async getUsers() {
     const users = await this.prisma.user.findMany({
       select: {
@@ -13,5 +14,19 @@ export class UserService {
       },
     });
     return users;
+  }
+
+  async getUser({ userId }: { userId: string }) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        id: true,
+        email: true,
+        firstName: true,
+      },
+    });
+    return user;
   }
 }
